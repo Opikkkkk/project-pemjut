69bbacb8a7b5019c78b565ff303fe7755af7704f
Opikkkkk <muhammadtaufiqrahmanhakim@gmail.com>
2025-06-04 20:27:29 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\RoleController;
+use Illuminate\Foundation\Application;
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+use App\Http\Controllers\API\AuthController;
+
+Route::get('/', function () {
+    return redirect()->route('login');
+});
+
+// Route::post('/login', [AuthController::class, 'login'])->name('login');
+
+
+Route::prefix('roles')->group(function () {
+    Route::get('/', [RoleController::class, 'index'])->name('roles');
+    Route::post('/', [RoleController::class, 'store'])->name('roles');
+    Route::post('/{id}', [RoleController::class, 'update'])->name('roles.update');
+    Route::delete('/{id}', [RoleController::class, 'destroy'])->name('roles.destroy');
+});
+
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__ . '/auth.php';
